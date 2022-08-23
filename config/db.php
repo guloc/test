21f853fc20878346fb684c5f457129aa797077da
guloc <adgali@rambler.ru>
2022-08-23 20:49:21 +0300

    Добавили новый бд

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2',
-    'username' => 'root',
-    'password' => 'root',
+    'username' => 'yii2test',
+    'password' => 'nN6jE2oN4k',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
